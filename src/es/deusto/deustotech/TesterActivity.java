3ad3504be7b774cf9fb4a7aaffcee631180c7301
edlectrico <edlectrico@gmail.com>
2013-07-23 16:48:18 +0200

    Adding dynamic text and buttons

--- src/es/deusto/deustotech/TesterActivity.java
+++ src/es/deusto/deustotech/TesterActivity.java
@@ -10,24 +10,113 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.Toast;
 
 public class TesterActivity extends Activity implements android.view.View.OnClickListener{
 
 	private static final String TAG = TesterActivity.class.getSimpleName();
-	private Button testButton;
+	private Button testButton1;
+	private Button testButton2;
 	private SharedPreferences minimunViewPreferences;
+	private GridLayout grid;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		testButton = (Button) findViewById(R.id.test_button);
-		testButton.setOnClickListener(this);
+		testButton1 = (Button) findViewById(R.id.test_button_1);
+		testButton1.setOnClickListener(this);
+		
+		testButton2 = (Button) findViewById(R.id.test_button_2);
+		testButton2.setOnClickListener(this);
 		
 		this.minimunViewPreferences = getSharedPreferences(getResources().getString(R.string.preferences_name_minui), 0);
+		
+		this.grid = (GridLayout) findViewById(R.id.default_layout);
+		
+		
+		this.grid.getChildAt(0).setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				Log.d(TAG, "Layout 0");
+				
+				return false;
+			}
+		});
+		
+		this.grid.getChildAt(1).setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				Log.d(TAG, "Layout 1");
+				
+				float x = event.getRawX();
+				float y = event.getRawY();
+		
+//				Log.d(TAG,"event.action" + event.getAction());
+//				Log.d(TAG, "Coordinates: X: " + x + "; Y: " + y);
+		
+				testButton1.setHeight((int) x);
+				testButton1.setWidth((int) y);
+				testButton1.invalidate();
+				
+				if (event.getAction() == MotionEvent.ACTION_UP){
+					//store
+					SharedPreferences.Editor uiEditor = minimunViewPreferences.edit();
+					Set<String> values = new HashSet<String>();
+					values.add(String.valueOf(x));
+					values.add(String.valueOf(y));
+					uiEditor.putStringSet(getResources().getString(R.string.adapted_configuration_ui), values);
+					uiEditor.commit();
+					
+					Log.d(TAG, "Stored!");
+				}
+				
+				return true;
+			}
+		});
+		
+		this.grid.getChildAt(2).setOnTouchListener(new OnTouchListener() {
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				Log.d(TAG, "Layout 2");
+				
+				float x = event.getRawX();
+				float y = event.getRawY();
+		
+//				Log.d(TAG,"event.action" + event.getAction());
+//				Log.d(TAG, "Coordinates: X: " + x + "; Y: " + y);
+		
+				testButton2.setHeight((int) x);
+				testButton2.setWidth((int) y);
+				
+				float textsize 		= testButton2.getTextSize();
+				float viewHeight 	= testButton2.getHeight();
+				
+				float proportion = viewHeight / textsize;
+
+				testButton2.invalidate();
+				testButton2.setTextSize(proportion);
+				
+				
+				if (event.getAction() == MotionEvent.ACTION_UP){
+					//store
+					SharedPreferences.Editor uiEditor = minimunViewPreferences.edit();
+					Set<String> values = new HashSet<String>();
+					values.add(String.valueOf(x));
+					values.add(String.valueOf(y));
+					uiEditor.putStringSet(getResources().getString(R.string.adapted_configuration_ui), values);
+					uiEditor.commit();
+					
+					Log.d(TAG, "Stored!");
+				}
+				
+				return true;
+			}
+		});
 	}
 
 	@Override
@@ -42,33 +131,30 @@ public class TesterActivity extends Activity implements android.view.View.OnClic
 		Toast.makeText(getApplicationContext(), "This is a test!", Toast.LENGTH_SHORT).show();
 	}
 
-	@Override
-	public boolean onTouchEvent(MotionEvent event) {
-		float x = event.getRawX();
-		float y = event.getRawY();
-
-		Log.d(TAG,"event.action" + event.getAction());
-		Log.d(TAG, "Coordinates: X: " + x + "; Y: " + y);
-
-		testButton.setHeight((int) x);
-		testButton.setWidth((int) y);
-		testButton.invalidate();
-		
-		if (event.getAction() == MotionEvent.ACTION_UP){
-			//store
-			SharedPreferences.Editor uiEditor = minimunViewPreferences.edit();
-			Set<String> values = new HashSet<String>();
-			values.add(String.valueOf(x));
-			values.add(String.valueOf(y));
-			uiEditor.putStringSet(getResources().getString(R.string.adapted_configuration_ui), values);
-			uiEditor.commit();
-			
-			Log.d(TAG, "Stored!");
-		}
-
-		return super.onTouchEvent(event);
-	}
-	 	
-	
-	
+//	@Override
+//	public boolean onTouchEvent(MotionEvent event) {
+//		float x = event.getRawX();
+//		float y = event.getRawY();
+//
+//		Log.d(TAG,"event.action" + event.getAction());
+//		Log.d(TAG, "Coordinates: X: " + x + "; Y: " + y);
+//
+//		testButton.setHeight((int) x);
+//		testButton.setWidth((int) y);
+//		testButton.invalidate();
+//		
+//		if (event.getAction() == MotionEvent.ACTION_UP){
+//			//store
+//			SharedPreferences.Editor uiEditor = minimunViewPreferences.edit();
+//			Set<String> values = new HashSet<String>();
+//			values.add(String.valueOf(x));
+//			values.add(String.valueOf(y));
+//			uiEditor.putStringSet(getResources().getString(R.string.adapted_configuration_ui), values);
+//			uiEditor.commit();
+//			
+//			Log.d(TAG, "Stored!");
+//		}
+//
+//		return super.onTouchEvent(event);
+//	}
 }
