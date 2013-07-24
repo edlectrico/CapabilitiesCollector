4056751c2d35c38e0bbba5cf7ff4dea6c8c8dac4
edlectrico <edlectrico@gmail.com>
2013-07-24 12:52:22 +0200

    Adding support for VoiceRecognition

--- src/es/deusto/deustotech/InputActivity.java
+++ src/es/deusto/deustotech/InputActivity.java
@@ -1,18 +1,22 @@
 package es.deusto.deustotech;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.View.OnLongClickListener;
-import android.widget.GridLayout;
 
 /**
  * @author edlectrico
@@ -20,34 +24,35 @@ import android.widget.GridLayout;
  */
 public class InputActivity extends Activity implements TextToSpeech.OnInitListener{
 
-	private GridLayout gl;
+	//variable for checking Voice Recognition support on user device
+	private static final int VR_REQUEST = 999;
+	
 	private TextToSpeech tts;
 	private Vibrator vibrator;
 	private boolean longPush = false;
+	private boolean voiceRecognition = false;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.input_activity);
 	
-		gl = (GridLayout) findViewById(R.id.grid_layout);
-		gl.setOnLongClickListener(new OnLongClickListener() {
-			
-			@Override
-			public boolean onLongClick(View v) {
-				longPush = true;
-				speakOut("If you prefer an audio based interaction, please say YES. If not, please say NO.");
-				return true;
-			}
-		});
+		voiceRecognition = checkVoiceRecognition();
 		
 		tts = new TextToSpeech(this, this);
 		vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
 		
+		findViewById(R.id.grid_layout).setOnLongClickListener(new OnLongClickListener() {
+			@Override
+			public boolean onLongClick(View v) {
+				onLongClickView();
+				return true;
+			}
+		});
+		
 		findViewById(R.id.input_button).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View view) {
-				//TODO: 1. Vibrate 2. Go to ViewsActivity
 				if (!longPush){
 					vibrator.vibrate(500);
 					speakOut("Well done!");
@@ -59,11 +64,22 @@ public class InputActivity extends Activity implements TextToSpeech.OnInitListen
 		findViewById(R.id.input_button).setOnLongClickListener(new OnLongClickListener() {
 			@Override
 			public boolean onLongClick(View view) {
-				longPush = true;
-				speakOut("If you prefer an audio based interaction, please say YES. If not, please say NO.");
+				onLongClickView();
 				return true;
 			}
 		});
+	}
+	
+	//Check if voice recognition is present
+	public boolean checkVoiceRecognition() {
+		PackageManager pm = getPackageManager();
+		List<ResolveInfo> activities = pm.queryIntentActivities(new Intent(
+				RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);
+		if (activities.size() == 0) {
+			Log.d(InputActivity.class.getSimpleName(), "Voice recognizer not present");
+			return false;
+		} 
+		return true;
 	}
 
 	@Override
@@ -88,5 +104,48 @@ public class InputActivity extends Activity implements TextToSpeech.OnInitListen
 	private void speakOut(final String text) {
         tts.speak(text, TextToSpeech.QUEUE_FLUSH, null);
     }
+	
+	private void onLongClickView() {
+		longPush = true;
+		speakOut("If you prefer an audio based interaction, please say YES. If not, please say NO.");
+		
+		if (voiceRecognition){
+			listenToSpeech();
+		}
+	}
+	
+	private void listenToSpeech() {
+		Intent listenIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+		listenIntent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass().getPackage().getName());
+		listenIntent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say a word!");
+		listenIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		listenIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 10);
+
+		//start listening
+		startActivityForResult(listenIntent, VR_REQUEST);
+	}
+	
+	public void speak(View view) {
+		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+		// Specify the calling package to identify your application
+		intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
+				.getPackage().getName());
+
+	}
+
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		//check speech recognition result 
+        if (requestCode == VR_REQUEST && resultCode == RESULT_OK) {
+        	//store the returned word list as an ArrayList
+            ArrayList<String> suggestedWords = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+            if (suggestedWords.contains("yes")){
+            	//TODO
+            } else if (suggestedWords.contains("no")){
+            	//TODO
+            }
+        }
+	}
 
 }
