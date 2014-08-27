9e3b03f20dcc67960f8ef8e1c1e1cd6835c4e706
edlectrico <edlectrico@gmail.com>
2014-08-27 11:22:49 +0200

    Removing comment

--- src/es/deusto/deustotech/capabilities/views/VolumeConfigActivity.java
+++ src/es/deusto/deustotech/capabilities/views/VolumeConfigActivity.java
@@ -249,11 +249,11 @@ public class VolumeConfigActivity extends AbstractActivity implements TextToSpee
 		getOntologyManager().addDataTypePropertyValue(getDisplays().get(0), 	getOntologyNamespace() + "displayHasBrightness", 	userPrefs.getBrightness());
 		
 		//TODO: remove
-		System.out.println("Checking Brightness");
-		final Collection<OWLLiteral> lightConditions = getOntologyManager().getDataTypePropertyValue(getDisplays().get(0), getOntologyNamespace() + "displayHasBrightness");
-		for (OWLLiteral brightness : lightConditions){
-			System.out.println("VolumeActivity: " + String.valueOf(((OWLLiteral) brightness).getLiteral()));
-		}
+//		System.out.println("Checking Brightness");
+//		final Collection<OWLLiteral> lightConditions = getOntologyManager().getDataTypePropertyValue(getDisplays().get(0), getOntologyNamespace() + "displayHasBrightness");
+//		for (OWLLiteral brightness : lightConditions){
+//			System.out.println("VolumeActivity: " + String.valueOf(((OWLLiteral) brightness).getLiteral()));
+//		}
 		
 		getOntologyManager().addDataTypePropertyValue(getLights().get(0), 		getOntologyNamespace() + "contextHasLight", 		userPrefs.getLuxes());
 		getOntologyManager().addDataTypePropertyValue(getEditTexts().get(0), 	getOntologyNamespace() + "viewHasColor", 			userPrefs.getEditTextBackgroundColor());
